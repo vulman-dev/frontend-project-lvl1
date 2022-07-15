b89db1dfde81f01de46a53a09f720d593a45b41e
Евгений Вульман <vulman@DESKTOP-0SKP5G9.localdomain>
2022-07-15 09:12:50 +0300

    new fix hexlet-check

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 // eslint-disable-next-line import/extensions
-import { getUserNameAndGreeting } from '../src/cli.js';
+import getUserNameAndGreeting from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 getUserNameAndGreeting();
